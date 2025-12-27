de7557c5ad7aec82faba82b291971fdfc9a57f99
unknown <dhruvikalariya011@gmail.com>
2025-12-28 00:22:15 +0530

    api end point for user

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+import userRouter from "./routes/user.routes.js"
+
+
 const app = express()
 
 app.use(cors({
@@ -13,6 +16,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+app.use("/api/v1/users", userRouter)
+
+
+
 
 
 export { app }                  
---
+++ src/routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import registerUser from "../controllers/user.controller.js";
+
+const router = Router()
+console.log("its here",registerUser);
+
+// router.route("/register").post(registerUser)
+// router.route("/register").post(registerUser)
+router.post("/register", registerUser)
+
+export default router
+// if we use defalut then we can import it with any name , import abc from "xyz.js"  || but if we use {xyz} then in export we must import it like , import {xyz} from"xyz.js
