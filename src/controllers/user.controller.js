180d19d96cca18e5e4612a792e1fc159299b1526
unknown <dhruvikalariya011@gmail.com>
2025-12-28 10:48:49 +0530

    user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,80 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+
+const registerUser = asyncHandler(async (req, res) => {
+
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+    const { username, email, fullname, password } = req.body
+    console.log("all the information : ", username)
+
+    if (!username || !email || !fullname || !password) {
+        throw new ApiError(
+            400,
+            "username, fullname, email and password are REQUIRED!!!",
+        )
+
+        // return res.status(400).json({
+        //     success: false,
+        //     message: "Name, email, and password are required",
+        // });
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
-} )
+    if(existedUser){
+        throw new ApiError(409, "User already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    const coverImage  = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar is Required")
+    }
+
+    const user = await User.create({
+        fullname, 
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    }) 
+    
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registring user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+
+})
 
 export default registerUser
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router()
 console.log("its here",registerUser);
 
 // router.route("/register").post(registerUser)
 // router.route("/register").post(registerUser)
-router.post("/register", registerUser)
+router.post("/register",upload.fields([
+    {
+        name: "avatar",
+        maxCount: 1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    }
+]), registerUser)
 
 export default router
 // if we use defalut then we can import it with any name , import abc from "xyz.js"  || but if we use {xyz} then in export we must import it like , import {xyz} from"xyz.js
