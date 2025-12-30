afb518361250cfdec8f5b006b50cd9d05f559d17
unknown <dhruvikalariya011@gmail.com>
2025-12-30 09:46:45 +0530

    auth middlewarw again

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 console.log("its here",registerUser);
@@ -17,6 +18,10 @@ router.post("/register",upload.fields([
         maxCount: 1
     }
 ]), registerUser)
+
+router.post("/login", loginUser)
+
+router.post("/logout", verifyJWT, logoutUser)
 
 export default router
 // if we use defalut then we can import it with any name , import abc from "xyz.js"  || but if we use {xyz} then in export we must import it like , import {xyz} from"xyz.js
